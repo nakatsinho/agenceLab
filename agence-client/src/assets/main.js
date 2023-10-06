0c931997db22d8c206f8306ac00721d594b6b3d6
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-10-06 14:23:03 +0200

    Fix: Trying to load in production

--- agence-client/src/assets/main.js
+++ agence-client/src/assets/main.js
@@ -1,3 +1,6 @@
-import './plugins/jquery/jquery.min.js';
-import './js/jquery.slimscroll.js';
+// import './plugins/jquery/jquery.min.js';
+import jquery from 'jquery';
+// import './js/jquery.slimscroll.js';
 // import './js/dashboard1.js';
+
+export default jquery;
