df03bfa0e40413e9a5321ba8052cd17b7c207172
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-10-09 21:55:58 +0200

    Fix: Update base path url

--- agence-client/src/services/http.js
+++ agence-client/src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://27c0-197-249-39-194.ngrok-free.app/api/',
+  baseURL: 'https://527c-197-235-53-177.ngrok-free.app/api/',
   headers: {
     'Content-type': 'application/json'
   }
